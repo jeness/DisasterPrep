9665e4e693f07300d2deadf0650830206dc66513
jeness <yuhaoran9968@gmail.com>
2019-02-17 15:35:47 -0500

    done

--- Assets/MyData/Scripts/PlayerWalk.cs
+++ Assets/MyData/Scripts/PlayerWalk.cs
@@ -11,10 +11,15 @@ public class PlayerWalk : MonoBehaviour {
     public GameObject wall;
     public int count;
     public GameObject standHere;
+    public GameObject myPlayer;
 
     void start()
     {
         standHere.SetActive(false);
+        //if(myPlayer == null)
+        //{
+        //    myPlayer = GameObject.FindWithTag("Player");
+        //}
     }
 
     // Update is called once per frame
@@ -23,6 +28,22 @@ public class PlayerWalk : MonoBehaviour {
         if (Input.GetButton("Fire1"))
         {
             transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+        var vel = myPlayer.GetComponent<Rigidbody>().velocity;
+        AudioSource footstep = GetComponent<AudioSource>();
+        if (vel.magnitude != 0 && footstep.isPlaying == false)
+       // if (vel.magnitude != 0)
+        {
+            //footstep.volume = Random.Range(0.8f, 1);
+            //footstep.pitch = Random.Range(0.8f, 1.1f);
+            footstep.Play();
+            footstep.enabled = true;
+            footstep.loop = true;
+        }
+        if(vel.magnitude == 0)
+        {
+            footstep.enabled = false;
+            footstep.loop = false;
         }
     }
 
@@ -33,7 +54,7 @@ public class PlayerWalk : MonoBehaviour {
             Destroy(other.gameObject);
             count++;
             totalCoins.text = count.ToString();
-            if (count == 9)
+            if (count == 10)
             {
                 wall.SetActive(false);
                 standHere.SetActive(true);
