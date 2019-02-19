3d788737b3a04ea46737763ac163532397f174c4
jeness <yuhaoran9968@gmail.com>
2019-02-19 02:39:25 -0500

    final v2.1

---
+++ Assets/MyData/Scripts/changeImage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeImage : MonoBehaviour {
+
+    // Use this for initialization
+    private GameObject[] images;
+    private int len;
+    
+    // Starting in 0 seconds.
+    // a projectile will be launched every 0.3 seconds
+    void Start()
+    {
+        //if(images == null)
+        //{
+            
+            images = GameObject.FindGameObjectsWithTag("TVImage");
+            len = images.Length;
+        //}
+
+        
+        InvokeRepeating("LaunchProjectile", 0f, 1.5f);
+    }
+
+    void LaunchProjectile()
+    {
+        foreach (GameObject image in images)
+        {
+            image.SetActive(false);
+        }
+        int index = Random.Range(0, len);
+        images[index].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
